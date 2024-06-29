66a7821193c3bb38eecd6be570aca7834b703280
Simon K <jackofspaces@gmail.com>
2024-06-29 14:06:01 +0100

    implement `Add()`

--- hashset.go
+++ hashset.go
@@ -1,5 +1,11 @@
 package set
 
 // Set is a generic implementation of a Hashset.
-type Set[T any] struct {
+type Set[T comparable] struct {
+	store map[T]struct{}
+}
+
+// Add adds the specified element into the set
+func (s *Set[T]) Add(element T) {
+	s.store[element] = struct{}{}
 }
