658957674d44734b190eda7c57be6e2699f2c76d
Simon K <jackofspaces@gmail.com>
2024-06-30 15:15:22 +0100

    add unit tests for covering `.Equals()` with different set lengths

--- hashset_test.go
+++ hashset_test.go
@@ -167,3 +167,9 @@ func TestUpdateMultiple(t *testing.T) {
 	expected := New(18, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
 	assert.True(t, a.Equals(expected))
 }
+
+func TestDifferentLensEqualsFalse(t *testing.T) {
+	a := New(0, 1, 2, 3)
+	b := New(0, 1, 2)
+	assert.False(t, a.Equals((b)))
+}
